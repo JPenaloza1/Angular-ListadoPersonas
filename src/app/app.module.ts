1b4c2f669ce059f1ec3d0d09859a34b73a41d88f
JPenaloza1 <100963782+JPenaloza1@users.noreply.github.com>
2022-10-18 23:58:29 -0500

    Apuntes 18/10/22

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { PersonaComponent } from './persona/persona.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { LoggingService } from './LoggingService.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PersonaComponent,
+    FormularioComponent
+  ],
+  imports: [
+    BrowserModule, FormsModule
+  ],
+  providers: [LoggingService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
